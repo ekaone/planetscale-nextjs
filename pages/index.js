d74b979098af292e39ac3482ca826541e06ea59c
ekaone <ekaone3033@gmail.com>
2022-12-22 17:29:45 +0700

    upgrade depedencies

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
-import Head from 'next/head';
-import Product from '../components/Product';
-import prisma from '../lib/prisma';
+import Head from "next/head";
+import Link from "next/link";
+import Product from "../components/Product";
+import Layout from "../components/Layout";
+import prisma from "../lib/prisma";
 
 export default function Home({ products }) {
   return (
@@ -10,19 +12,29 @@ export default function Home({ products }) {
         <meta name="description" content="PlanetScale Quickstart for Next.js" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className="p-10 mx-auto max-w-4xl">
-        <h1 className="text-6xl font-bold mb-4 text-center">Next.js Starter</h1>
-        <p className="mb-20 text-xl text-center">
-          🔥 Shop from the hottest items in the world 🔥
-        </p>
-        <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 justify-items-center  gap-4">
-          {products.map((product) => (
-            <Product product={product} key={product.id} />
-          ))}
-        </div>
+        <Layout>
+          <h1 className="text-6xl font-bold mb-4 text-center">
+            Next.js Starter
+          </h1>
+          <p className="mb-20 text-xl text-center">
+            🔥 Shop from the hottest items in the world 🔥
+          </p>
+          <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 justify-items-center gap-10">
+            {products.map((product) => (
+              <Link
+                href={`/photo/${product.id}`}
+                scroll={false}
+                key={product.id}
+              >
+                <a className="overflow-hidden flex rounded-2xl shadow-xl hover:scale-105 hover:shadow-2xl hover:z-10 transition-all ease-in-out">
+                  <Product product={product} key={product.id} />
+                </a>
+              </Link>
+            ))}
+          </div>
+        </Layout>
       </main>
-
       <footer></footer>
     </div>
   );
